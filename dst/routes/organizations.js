f703ca16e3ae443854e7a2084a525ba95f139bfd
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-09-10 08:38:11 +0000

    # CINERINO-2 ### Changed - 組織管理を強化。

--- dst/routes/organizations.js
+++ dst/routes/organizations.js
@@ -15,6 +15,7 @@ const chevreapi = require("@chevre/api-nodejs-client");
 const cinerinoapi = require("@cinerino/api-nodejs-client");
 const createDebug = require("debug");
 const express = require("express");
+const http_status_1 = require("http-status");
 const debug = createDebug('cinerino-console:routes');
 const chevreAuthClient = new chevreapi.auth.ClientCredentials({
     domain: process.env.CHEVRE_AUTHORIZE_SERVER_DOMAIN,
@@ -62,63 +63,30 @@ organizationsRouter.get('/movieTheater', (req, res, next) => __awaiter(this, voi
  */
 organizationsRouter.all('/movieTheater/new', (req, res, next) => __awaiter(this, void 0, void 0, function* () {
     try {
-        throw new Error('implementing...');
-        // @ts-ignore: Unreachable code error
         let message;
+        let attributes;
         if (req.method === 'POST') {
             try {
-                // Chevreから情報取得
-                const placeService = new chevreapi.service.Place({
-                    endpoint: process.env.CHEVRE_ENDPOINT,
-                    auth: chevreAuthClient
+                attributes = yield createAttributesFromBody({ body: req.body });
+                debug('creating organization...', attributes);
+                const organizationService = new cinerinoapi.service.Organization({
+                    endpoint: process.env.API_ENDPOINT,
+                    auth: req.user.authClient
                 });
-                const movieTheaterFromChevre = yield placeService.findMovieTheaterByBranchCode({ branchCode: req.body.branchCode });
-                const movieTheater = {
-                    id: '',
-                    typeOf: cinerinoapi.factory.organizationType.MovieTheater,
-                    identifier: `${cinerinoapi.factory.organizationType.MovieTheater}-${req.body.branchCode}`,
-                    name: movieTheaterFromChevre.name,
-                    legalName: movieTheaterFromChevre.name,
-                    parentOrganization: {
-                        name: {
-                            en: 'Motionpicture Co., Ltd.',
-                            ja: '株式会社モーションピクチャー'
-                        },
-                        identifier: 'Motionpicture',
-                        typeOf: cinerinoapi.factory.organizationType.Corporation
-                    },
-                    location: {
-                        typeOf: movieTheaterFromChevre.typeOf,
-                        branchCode: movieTheaterFromChevre.branchCode,
-                        name: movieTheaterFromChevre.name
-                    },
-                    telephone: movieTheaterFromChevre.telephone,
-                    url: req.body.url,
-                    paymentAccepted: [
-                        {
-                            paymentMethodType: cinerinoapi.factory.paymentMethodType.CreditCard,
-                            gmoInfo: {
-                                siteId: process.env.GMO_SITE_ID,
-                                shopId: req.body['gmoInfo.shopId'],
-                                shopPass: req.body['gmoInfo.shopPass']
-                            }
-                        }
-                    ]
-                };
-                debug('creating organization...', movieTheater);
-                // const doc = await organizationRepo.organizationModel.create(movieTheater);
-                // movieTheater = doc.toObject();
-                // req.flash('message', '劇場を作成しました。');
-                // res.redirect(`/organizations/movieTheater/${movieTheater.id}`);
-                res.redirect('/organizations/movieTheater/new');
+                const movieTheater = yield organizationService.openMovieTheater(attributes);
+                req.flash('message', '販売者を作成しました');
+                res.redirect(`/organizations/movieTheater/${movieTheater.id}`);
                 return;
             }
             catch (error) {
+                debug(error);
                 message = error.message;
             }
         }
         res.render('organizations/movieTheater/new', {
-            message: message
+            message: message,
+            attributes: attributes,
+            PaymentMethodType: cinerinoapi.factory.paymentMethodType
         });
     }
     catch (error) {
@@ -128,72 +96,106 @@ organizationsRouter.all('/movieTheater/new', (req, res, next) => __awaiter(this,
 /**
  * 販売者編集
  */
-organizationsRouter.all('/movieTheater/:id', (_, __, next) => __awaiter(this, void 0, void 0, function* () {
+organizationsRouter.all('/movieTheater/:id', (req, res, next) => __awaiter(this, void 0, void 0, function* () {
     try {
-        throw new Error('implementing...');
-        // let message;
-        // const organizationService = new cinerinoapi.service.Organization({
-        //     endpoint: <string>process.env.API_ENDPOINT,
-        //     auth: req.user.authClient
-        // });
-        // const organizationRepo = new cinerino.repository.Organization(cinerino.mongoose.connection);
-        // const doc = await organizationRepo.organizationModel.findById(req.params.id).exec();
-        // if (doc === null) {
-        //     throw new cinerino.factory.errors.NotFound('Movie theater');
-        // }
-        // const movieTheater = <cinerino.factory.organization.movieTheater.IOrganization>doc.toObject();
-        // if (Array.isArray(movieTheater.paymentAccepted) &&
-        //    movieTheater.paymentAccepted.find(
-        //        (p) => p.paymentMethodType === cinerino.factory.paymentMethodType.Pecorino) !== undefined
-        //    ) {
-        //     (<any>movieTheater).pecorinoPaymentAccepted = 'on';
-        // }
-        // if (req.method === 'POST') {
-        //     try {
-        //         const update = req.body;
-        //         if (!Array.isArray(movieTheater.paymentAccepted)) {
-        //             movieTheater.paymentAccepted = [];
-        //         }
-        //         update.paymentAccepted = movieTheater.paymentAccepted;
-        //         // ポイント決済を有効にする場合、口座未開設であれば開設する
-        //         if (update.pecorinoPaymentAccepted === 'on') {
-        //             // tslint:disable-next-line:max-line-length
-        //            if (movieTheater.paymentAccepted.find(
-        //                (p) => p.paymentMethodType === cinerino.factory.paymentMethodType.Pecorino) === undefined
-        //            ) {
-        //                 const account = await cinerino.service.account.open({
-        //                     name: movieTheater.name.ja
-        //                 })({
-        //                     accountNumber: new cinerino.repository.AccountNumber(redisClient),
-        //                     accountService: new cinerino.pecorinoapi.service.Account({
-        //                         endpoint: <string>process.env.PECORINO_API_ENDPOINT,
-        //                         auth: pecorinoAuthClient
-        //                     })
-        //                 });
-        //                 debug('account opened.');
-        //                 update.paymentAccepted.push({
-        //                     paymentMethodType: cinerino.factory.paymentMethodType.Pecorino,
-        //                     accountNumber: account.accountNumber
-        //                 });
-        //             }
-        //         }
-        //         debug('updating movie theater:', update);
-        //         await organizationRepo.organizationModel.findByIdAndUpdate(movieTheater.id, update).exec();
-        //         debug('movie theater updated.');
-        //         req.flash('message', '更新しました。');
-        //         res.redirect(req.originalUrl);
-        //         return;
-        //     } catch (error) {
-        //         message = error.message;
-        //     }
-        // }
-        // res.render('organizations/movieTheater/edit', {
-        //     message: message
-        //     // movieTheater: movieTheater
-        // });
+        let message;
+        let attributes;
+        const organizationService = new cinerinoapi.service.Organization({
+            endpoint: process.env.API_ENDPOINT,
+            auth: req.user.authClient
+        });
+        const movieTheater = yield organizationService.findMovieTheaterById({ id: req.params.id });
+        if (req.method === 'DELETE') {
+            yield organizationService.deleteMovieTheaterById({ id: req.params.id });
+            res.status(http_status_1.NO_CONTENT).end();
+            return;
+        }
+        else if (req.method === 'POST') {
+            try {
+                attributes = yield createAttributesFromBody({ body: req.body });
+                // if (!Array.isArray(movieTheater.paymentAccepted)) {
+                //     movieTheater.paymentAccepted = [];
+                // }
+                // update.paymentAccepted = movieTheater.paymentAccepted;
+                // // ポイント決済を有効にする場合、口座未開設であれば開設する
+                // if (update.pecorinoPaymentAccepted === 'on') {
+                //     // tslint:disable-next-line:max-line-length
+                //    if (movieTheater.paymentAccepted.find(
+                //        (p) => p.paymentMethodType === cinerino.factory.paymentMethodType.Pecorino) === undefined
+                //    ) {
+                //         const account = await cinerino.service.account.open({
+                //             name: movieTheater.name.ja
+                //         })({
+                //             accountNumber: new cinerino.repository.AccountNumber(redisClient),
+                //             accountService: new cinerino.pecorinoapi.service.Account({
+                //                 endpoint: <string>process.env.PECORINO_API_ENDPOINT,
+                //                 auth: pecorinoAuthClient
+                //             })
+                //         });
+                //         debug('account opened.');
+                //         update.paymentAccepted.push({
+                //             paymentMethodType: cinerino.factory.paymentMethodType.Pecorino,
+                //             accountNumber: account.accountNumber
+                //         });
+                //     }
+                // }
+                yield organizationService.updateMovieTheaterById({ id: req.params.id, attributes: attributes });
+                req.flash('message', '更新しました');
+                res.redirect(req.originalUrl);
+                return;
+            }
+            catch (error) {
+                message = error.message;
+            }
+        }
+        res.render('organizations/movieTheater/edit', {
+            message: message,
+            movieTheater: movieTheater,
+            PaymentMethodType: cinerinoapi.factory.paymentMethodType
+        });
     }
     catch (error) {
         next(error);
     }
 }));
+function createAttributesFromBody(params) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Chevreから情報取得
+        const placeService = new chevreapi.service.Place({
+            endpoint: process.env.CHEVRE_ENDPOINT,
+            auth: chevreAuthClient
+        });
+        const movieTheaterFromChevre = yield placeService.findMovieTheaterByBranchCode({ branchCode: params.body.branchCode });
+        return {
+            typeOf: cinerinoapi.factory.organizationType.MovieTheater,
+            name: movieTheaterFromChevre.name,
+            legalName: movieTheaterFromChevre.name,
+            parentOrganization: {
+                name: {
+                    en: 'Motionpicture Co., Ltd.',
+                    ja: '株式会社モーションピクチャー'
+                },
+                identifier: 'Motionpicture',
+                typeOf: cinerinoapi.factory.organizationType.Corporation
+            },
+            location: {
+                typeOf: movieTheaterFromChevre.typeOf,
+                branchCode: movieTheaterFromChevre.branchCode,
+                name: movieTheaterFromChevre.name
+            },
+            telephone: movieTheaterFromChevre.telephone,
+            url: params.body.url,
+            paymentAccepted: [
+                {
+                    paymentMethodType: cinerinoapi.factory.paymentMethodType.CreditCard,
+                    gmoInfo: {
+                        siteId: process.env.GMO_SITE_ID,
+                        shopId: params.body['gmoInfo.shopId'],
+                        shopPass: params.body['gmoInfo.shopPass']
+                    }
+                }
+            ]
+        };
+    });
+}
 exports.default = organizationsRouter;
--- src/routes/organizations.ts
+++ src/routes/organizations.ts
@@ -5,6 +5,7 @@ import * as chevreapi from '@chevre/api-nodejs-client';
 import * as cinerinoapi from '@cinerino/api-nodejs-client';
 import * as createDebug from 'debug';
 import * as express from 'express';
+import { NO_CONTENT } from 'http-status';
 
 const debug = createDebug('cinerino-console:routes');
 const chevreAuthClient = new chevreapi.auth.ClientCredentials({
@@ -47,7 +48,8 @@ organizationsRouter.get(
         } catch (error) {
             next(error);
         }
-    });
+    }
+);
 /**
  * 販売者追加
  */
@@ -55,143 +57,142 @@ organizationsRouter.all(
     '/movieTheater/new',
     async (req, res, next) => {
         try {
-            throw new Error('implementing...');
-            // @ts-ignore: Unreachable code error
             let message;
+            let attributes: cinerinoapi.factory.organization.IAttributes<cinerinoapi.factory.organizationType.MovieTheater> | undefined;
             if (req.method === 'POST') {
                 try {
-                    // Chevreから情報取得
-                    const placeService = new chevreapi.service.Place({
-                        endpoint: <string>process.env.CHEVRE_ENDPOINT,
-                        auth: chevreAuthClient
+                    attributes = await createAttributesFromBody({ body: req.body });
+                    debug('creating organization...', attributes);
+                    const organizationService = new cinerinoapi.service.Organization({
+                        endpoint: <string>process.env.API_ENDPOINT,
+                        auth: req.user.authClient
                     });
-                    const movieTheaterFromChevre = await placeService.findMovieTheaterByBranchCode({ branchCode: req.body.branchCode });
-                    const movieTheater: cinerinoapi.factory.organization.movieTheater.IOrganization = {
-                        id: '',
-                        typeOf: cinerinoapi.factory.organizationType.MovieTheater,
-                        identifier: `${cinerinoapi.factory.organizationType.MovieTheater}-${req.body.branchCode}`,
-                        name: movieTheaterFromChevre.name,
-                        legalName: movieTheaterFromChevre.name,
-                        parentOrganization: {
-                            name: {
-                                en: 'Motionpicture Co., Ltd.',
-                                ja: '株式会社モーションピクチャー'
-                            },
-                            identifier: 'Motionpicture',
-                            typeOf: cinerinoapi.factory.organizationType.Corporation
-                        },
-                        location: {
-                            typeOf: movieTheaterFromChevre.typeOf,
-                            branchCode: movieTheaterFromChevre.branchCode,
-                            name: movieTheaterFromChevre.name
-                        },
-                        telephone: movieTheaterFromChevre.telephone,
-                        url: req.body.url,
-                        paymentAccepted: [
-                            {
-                                paymentMethodType: cinerinoapi.factory.paymentMethodType.CreditCard,
-                                gmoInfo: {
-                                    siteId: <string>process.env.GMO_SITE_ID,
-                                    shopId: req.body['gmoInfo.shopId'],
-                                    shopPass: req.body['gmoInfo.shopPass']
-                                }
-                            }
-                        ]
-                    };
-                    debug('creating organization...', movieTheater);
-                    // const doc = await organizationRepo.organizationModel.create(movieTheater);
-                    // movieTheater = doc.toObject();
-                    // req.flash('message', '劇場を作成しました。');
-                    // res.redirect(`/organizations/movieTheater/${movieTheater.id}`);
-                    res.redirect('/organizations/movieTheater/new');
+                    const movieTheater = await organizationService.openMovieTheater(attributes);
+                    req.flash('message', '販売者を作成しました');
+                    res.redirect(`/organizations/movieTheater/${movieTheater.id}`);
+
+                    return;
+                } catch (error) {
+                    debug(error);
+                    message = error.message;
+                }
+            }
+            res.render('organizations/movieTheater/new', {
+                message: message,
+                attributes: attributes,
+                PaymentMethodType: cinerinoapi.factory.paymentMethodType
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+/**
+ * 販売者編集
+ */
+organizationsRouter.all(
+    '/movieTheater/:id',
+    async (req, res, next) => {
+        try {
+            let message;
+            let attributes: cinerinoapi.factory.organization.IAttributes<cinerinoapi.factory.organizationType.MovieTheater> | undefined;
+            const organizationService = new cinerinoapi.service.Organization({
+                endpoint: <string>process.env.API_ENDPOINT,
+                auth: req.user.authClient
+            });
+            const movieTheater = await organizationService.findMovieTheaterById({ id: req.params.id });
+            if (req.method === 'DELETE') {
+                await organizationService.deleteMovieTheaterById({ id: req.params.id });
+                res.status(NO_CONTENT).end();
+
+                return;
+            } else if (req.method === 'POST') {
+                try {
+                    attributes = await createAttributesFromBody({ body: req.body });
+                    // if (!Array.isArray(movieTheater.paymentAccepted)) {
+                    //     movieTheater.paymentAccepted = [];
+                    // }
+                    // update.paymentAccepted = movieTheater.paymentAccepted;
+                    // // ポイント決済を有効にする場合、口座未開設であれば開設する
+                    // if (update.pecorinoPaymentAccepted === 'on') {
+                    //     // tslint:disable-next-line:max-line-length
+                    //    if (movieTheater.paymentAccepted.find(
+                    //        (p) => p.paymentMethodType === cinerino.factory.paymentMethodType.Pecorino) === undefined
+                    //    ) {
+                    //         const account = await cinerino.service.account.open({
+                    //             name: movieTheater.name.ja
+                    //         })({
+                    //             accountNumber: new cinerino.repository.AccountNumber(redisClient),
+                    //             accountService: new cinerino.pecorinoapi.service.Account({
+                    //                 endpoint: <string>process.env.PECORINO_API_ENDPOINT,
+                    //                 auth: pecorinoAuthClient
+                    //             })
+                    //         });
+                    //         debug('account opened.');
+                    //         update.paymentAccepted.push({
+                    //             paymentMethodType: cinerino.factory.paymentMethodType.Pecorino,
+                    //             accountNumber: account.accountNumber
+                    //         });
+                    //     }
+                    // }
+                    await organizationService.updateMovieTheaterById({ id: req.params.id, attributes: attributes });
+                    req.flash('message', '更新しました');
+                    res.redirect(req.originalUrl);
 
                     return;
                 } catch (error) {
                     message = error.message;
                 }
             }
-            res.render('organizations/movieTheater/new', {
-                message: message
+            res.render('organizations/movieTheater/edit', {
+                message: message,
+                movieTheater: movieTheater,
+                PaymentMethodType: cinerinoapi.factory.paymentMethodType
             });
         } catch (error) {
             next(error);
         }
+    }
+);
+async function createAttributesFromBody(params: {
+    body: any;
+}): Promise<cinerinoapi.factory.organization.IAttributes<cinerinoapi.factory.organizationType.MovieTheater>> {
+    // Chevreから情報取得
+    const placeService = new chevreapi.service.Place({
+        endpoint: <string>process.env.CHEVRE_ENDPOINT,
+        auth: chevreAuthClient
     });
-/**
- * 販売者編集
- */
-organizationsRouter.all(
-    '/movieTheater/:id',
-    async (_, __, next) => {
-        try {
-            throw new Error('implementing...');
-            // let message;
-            // const organizationService = new cinerinoapi.service.Organization({
-            //     endpoint: <string>process.env.API_ENDPOINT,
-            //     auth: req.user.authClient
-            // });
-            // const organizationRepo = new cinerino.repository.Organization(cinerino.mongoose.connection);
+    const movieTheaterFromChevre = await placeService.findMovieTheaterByBranchCode({ branchCode: params.body.branchCode });
 
-            // const doc = await organizationRepo.organizationModel.findById(req.params.id).exec();
-            // if (doc === null) {
-            //     throw new cinerino.factory.errors.NotFound('Movie theater');
-            // }
-            // const movieTheater = <cinerino.factory.organization.movieTheater.IOrganization>doc.toObject();
-            // if (Array.isArray(movieTheater.paymentAccepted) &&
-            //    movieTheater.paymentAccepted.find(
-            //        (p) => p.paymentMethodType === cinerino.factory.paymentMethodType.Pecorino) !== undefined
-            //    ) {
-            //     (<any>movieTheater).pecorinoPaymentAccepted = 'on';
-            // }
-
-            // if (req.method === 'POST') {
-            //     try {
-            //         const update = req.body;
-            //         if (!Array.isArray(movieTheater.paymentAccepted)) {
-            //             movieTheater.paymentAccepted = [];
-            //         }
-            //         update.paymentAccepted = movieTheater.paymentAccepted;
-
-            //         // ポイント決済を有効にする場合、口座未開設であれば開設する
-            //         if (update.pecorinoPaymentAccepted === 'on') {
-            //             // tslint:disable-next-line:max-line-length
-            //            if (movieTheater.paymentAccepted.find(
-            //                (p) => p.paymentMethodType === cinerino.factory.paymentMethodType.Pecorino) === undefined
-            //            ) {
-            //                 const account = await cinerino.service.account.open({
-            //                     name: movieTheater.name.ja
-            //                 })({
-            //                     accountNumber: new cinerino.repository.AccountNumber(redisClient),
-            //                     accountService: new cinerino.pecorinoapi.service.Account({
-            //                         endpoint: <string>process.env.PECORINO_API_ENDPOINT,
-            //                         auth: pecorinoAuthClient
-            //                     })
-            //                 });
-            //                 debug('account opened.');
-            //                 update.paymentAccepted.push({
-            //                     paymentMethodType: cinerino.factory.paymentMethodType.Pecorino,
-            //                     accountNumber: account.accountNumber
-            //                 });
-            //             }
-            //         }
-            //         debug('updating movie theater:', update);
-            //         await organizationRepo.organizationModel.findByIdAndUpdate(movieTheater.id, update).exec();
-            //         debug('movie theater updated.');
-            //         req.flash('message', '更新しました。');
-            //         res.redirect(req.originalUrl);
-
-            //         return;
-            //     } catch (error) {
-            //         message = error.message;
-            //     }
-            // }
-
-            // res.render('organizations/movieTheater/edit', {
-            //     message: message
-            //     // movieTheater: movieTheater
-            // });
-        } catch (error) {
-            next(error);
-        }
-    });
+    return {
+        typeOf: cinerinoapi.factory.organizationType.MovieTheater,
+        name: movieTheaterFromChevre.name,
+        legalName: movieTheaterFromChevre.name,
+        parentOrganization: {
+            name: {
+                en: 'Motionpicture Co., Ltd.',
+                ja: '株式会社モーションピクチャー'
+            },
+            identifier: 'Motionpicture',
+            typeOf: cinerinoapi.factory.organizationType.Corporation
+        },
+        location: {
+            typeOf: movieTheaterFromChevre.typeOf,
+            branchCode: movieTheaterFromChevre.branchCode,
+            name: movieTheaterFromChevre.name
+        },
+        telephone: movieTheaterFromChevre.telephone,
+        url: params.body.url,
+        paymentAccepted: [
+            {
+                paymentMethodType: cinerinoapi.factory.paymentMethodType.CreditCard,
+                gmoInfo: {
+                    siteId: <string>process.env.GMO_SITE_ID,
+                    shopId: params.body['gmoInfo.shopId'],
+                    shopPass: params.body['gmoInfo.shopPass']
+                }
+            }
+        ]
+    };
+}
 export default organizationsRouter;
