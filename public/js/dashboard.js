78ad8f5d434c4e69ceb2875fb830b2666b4fdc50
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-10-01 15:27:53 +0000

    # CINERINO-2 ### Changed - ダッシュボードを調整。

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -11,6 +11,9 @@ var limit = 100;
 var page = 0;
 var numVisitorsChart;
 
+var initialChartStartDate = moment().subtract(29, 'days');
+var initialChartEndDate = moment();
+
 $(function () {
     'use strict'
 
@@ -44,38 +47,11 @@ $(function () {
             'This Month': [moment().startOf('month'), moment().endOf('month')],
             'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
         },
-        startDate: moment().subtract(29, 'days'),
-        endDate: moment()
+        startDate: initialChartStartDate,
+        endDate: initialChartEndDate
     }, function (start, end) {
         console.log('You chose: ' + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-        searchSalesAmount(
-            {
-                measureFrom: start.toDate(),
-                measureThrough: end.toDate()
-            },
-            createSalesAmountChart
-        );
-        searchSalesAmountByClient(
-            {
-                measureFrom: start.toDate(),
-                measureThrough: end.toDate()
-            },
-            createSalesAmountByClientChart
-        );
-        searchSalesAmountByPaymentMethod(
-            {
-                measureFrom: start.toDate(),
-                measureThrough: end.toDate()
-            },
-            createSalesAmountByPaymentMethodChart
-        );
-        searchSalesAmountBySeller(
-            {
-                measureFrom: start.toDate(),
-                measureThrough: end.toDate()
-            },
-            createSalesAmountBySellerChart
-        );
+        updateSalesAmountChart();
     });
 
     $('#numOrderItems .daterange').daterangepicker({
@@ -87,38 +63,11 @@ $(function () {
             'This Month': [moment().startOf('month'), moment().endOf('month')],
             'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
         },
-        startDate: moment().subtract(29, 'days'),
-        endDate: moment()
+        startDate: initialChartStartDate,
+        endDate: initialChartEndDate
     }, function (start, end) {
-        console.log('You chose: ' + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-        searchNumOrderItems(
-            {
-                measureFrom: start.toDate(),
-                measureThrough: end.toDate()
-            },
-            createNumOrderItemsChart
-        );
-        searchNumOrderItemsByClient(
-            {
-                measureFrom: start.toDate(),
-                measureThrough: end.toDate()
-            },
-            createNumOrderItemsByClientChart
-        );
-        searchNumOrderItemsByPaymentMethod(
-            {
-                measureFrom: start.toDate(),
-                measureThrough: end.toDate()
-            },
-            createNumOrderItemsByPaymentMethodChart
-        );
-        searchNumOrderItemsBySeller(
-            {
-                measureFrom: start.toDate(),
-                measureThrough: end.toDate()
-            },
-            createNumOrderItemsBySellerChart
-        );
+        console.log('You chose: ' + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        updateNumOrderItemsChart();
     });
 
     $('#numPlaceOrder .daterange').daterangepicker({
@@ -130,33 +79,29 @@ $(function () {
             'This Month': [moment().startOf('month'), moment().endOf('month')],
             'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
         },
-        startDate: moment().subtract(29, 'days'),
-        endDate: moment()
+        startDate: initialChartStartDate,
+        endDate: initialChartEndDate
     }, function (start, end) {
         console.log('You chose: ' + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
         searchNumPlaceOrder(
-            {
-                measureFrom: start.toDate(),
-                measureThrough: end.toDate()
-            },
             createNumPlaceOrderChart
         );
     });
 
     // jvectormap data
-    var visitorsData = {
-        'US': 398, //USA
-        'SA': 400, //Saudi Arabia
-        'CA': 1000, //Canada
-        'DE': 500, //Germany
-        'FR': 760, //France
-        'CN': 300, //China
-        'AU': 700, //Australia
-        'BR': 600, //Brazil
-        'IN': 800, //India
-        'GB': 320, //Great Britain
-        'RU': 3000 //Russia
-    }
+    // var visitorsData = {
+    //     'US': 398, //USA
+    //     'SA': 400, //Saudi Arabia
+    //     'CA': 1000, //Canada
+    //     'DE': 500, //Germany
+    //     'FR': 760, //France
+    //     'CN': 300, //China
+    //     'AU': 700, //Australia
+    //     'BR': 600, //Brazil
+    //     'IN': 800, //India
+    //     'GB': 320, //Great Britain
+    //     'RU': 3000 //Russia
+    // }
     // World map by jvectormap
     // $('#world-map').vectorMap({
     //     map: 'world_mill_en',
@@ -217,43 +162,6 @@ $(function () {
     //     height: '250px'
     // })
 
-    /* Morris.js Charts */
-    // Sales chart
-    // var area = new Morris.Area({
-    //     element: 'revenue-chart',
-    //     resize: true,
-    //     data: [
-    //         { y: '2011 Q1', item1: 2666, item2: 2666 },
-    //         { y: '2011 Q2', item1: 2778, item2: 2294 },
-    //         { y: '2011 Q3', item1: 4912, item2: 1969 },
-    //         { y: '2011 Q4', item1: 3767, item2: 3597 },
-    //         { y: '2012 Q1', item1: 6810, item2: 1914 },
-    //         { y: '2012 Q2', item1: 5670, item2: 4293 },
-    //         { y: '2012 Q3', item1: 4820, item2: 3795 },
-    //         { y: '2012 Q4', item1: 15073, item2: 5967 },
-    //         { y: '2013 Q1', item1: 10687, item2: 4460 },
-    //         { y: '2013 Q2', item1: 8432, item2: 5713 }
-    //     ],
-    //     xkey: 'y',
-    //     ykeys: ['item1', 'item2'],
-    //     labels: ['Item 1', 'Item 2'],
-    //     lineColors: ['#495057', '#007cff'],
-    //     hideHover: 'auto'
-    // })
-
-    // Donut Chart
-    // var donut = new Morris.Donut({
-    //     element: 'sales-chart',
-    //     resize: true,
-    //     colors: ['#007bff', '#dc3545', '#28a745'],
-    //     data: [
-    //         { label: 'Download Sales', value: 12 },
-    //         { label: 'In-Store Sales', value: 30 },
-    //         { label: 'Mail-Order Sales', value: 20 }
-    //     ],
-    //     hideHover: 'auto'
-    // })
-
     // Fix for charts under tabs
     $('.box ul.nav a').on('shown.bs.tab', function () {
         area.redraw()
@@ -269,79 +177,13 @@ $(function () {
     $('#numOrderItems .knob.byPaymentMethod').knob();
     $('#numOrderItems .knob.bySeller').knob();
 
-    countNewOrder(function () {
-    });
-    aggregateExitRate(function () {
-    });
-    countNewUser(function () {
-    });
-    countNewTransaction(function () {
-    });
-    searchSalesAmount(
-        {
-            measureFrom: moment().subtract(29, 'days').toDate(),
-            measureThrough: moment().toDate()
+    updateCharts();
+    setInterval(
+        function () {
+            updateCharts();
         },
-        createSalesAmountChart
+        60000
     );
-    searchSalesAmountByClient(
-        {
-            measureFrom: moment().subtract(29, 'days').toDate(),
-            measureThrough: moment().toDate()
-        },
-        createSalesAmountByClientChart
-    );
-    searchSalesAmountByPaymentMethod(
-        {
-            measureFrom: moment().subtract(29, 'days').toDate(),
-            measureThrough: moment().toDate()
-        },
-        createSalesAmountByPaymentMethodChart
-    );
-    searchSalesAmountBySeller(
-        {
-            measureFrom: moment().subtract(29, 'days').toDate(),
-            measureThrough: moment().toDate()
-        },
-        createSalesAmountBySellerChart
-    );
-    searchNumOrderItems(
-        {
-            measureFrom: moment().subtract(29, 'days').toDate(),
-            measureThrough: moment().toDate()
-        },
-        createNumOrderItemsChart
-    );
-    searchNumOrderItemsByClient(
-        {
-            measureFrom: moment().subtract(29, 'days').toDate(),
-            measureThrough: moment().toDate()
-        },
-        createNumOrderItemsByClientChart
-    );
-    searchNumOrderItemsByPaymentMethod(
-        {
-            measureFrom: moment().subtract(29, 'days').toDate(),
-            measureThrough: moment().toDate()
-        },
-        createNumOrderItemsByPaymentMethodChart
-    );
-    searchNumOrderItemsBySeller(
-        {
-            measureFrom: moment().subtract(29, 'days').toDate(),
-            measureThrough: moment().toDate()
-        },
-        createNumOrderItemsBySellerChart
-    );
-    searchNumPlaceOrder(
-        {
-            measureFrom: moment().subtract(29, 'days').toDate(),
-            measureThrough: moment().toDate()
-        },
-        createNumPlaceOrderChart
-    );
-    searchLatestOrders(function () {
-    });
 
     $.getJSON(
         WAITER_ENDPOINT + '/rules',
@@ -352,13 +194,58 @@ $(function () {
     }).fail(function () {
     });
 });
-function searchSalesAmount(params, cb) {
+function updateCharts() {
+    countNewOrder(function () {
+    });
+    aggregateExitRate(function () {
+    });
+    countNewUser(function () {
+    });
+    countNewTransaction(function () {
+    });
+    updateSalesAmountChart();
+    updateNumOrderItemsChart();
+    searchLatestOrders(function () {
+    });
+}
+function updateSalesAmountChart() {
+    searchSalesAmount(
+        createSalesAmountChart
+    );
+    searchSalesAmountByClient(
+        createSalesAmountByClientChart
+    );
+    searchSalesAmountByPaymentMethod(
+        createSalesAmountByPaymentMethodChart
+    );
+    searchSalesAmountBySeller(
+        createSalesAmountBySellerChart
+    );
+}
+function updateNumOrderItemsChart() {
+    searchNumOrderItems(
+        createNumOrderItemsChart
+    );
+    searchNumOrderItemsByClient(
+        createNumOrderItemsByClientChart
+    );
+    searchNumOrderItemsByPaymentMethod(
+        createNumOrderItemsByPaymentMethodChart
+    );
+    searchNumOrderItemsBySeller(
+        createNumOrderItemsBySellerChart
+    );
+    searchNumPlaceOrder(
+        createNumPlaceOrderChart
+    );
+}
+function searchSalesAmount(cb) {
     $('#salesAmount .overlay').show();
     $.getJSON(
         '/dashboard/telemetry/SalesAmount',
         {
-            measureFrom: moment(params.measureFrom).toISOString(),
-            measureThrough: moment(params.measureThrough).toISOString()
+            measureFrom: $('#salesAmount .daterange').data('daterangepicker').startDate.toISOString(),
+            measureThrough: $('#salesAmount .daterange').data('daterangepicker').endDate.toISOString()
         }
     ).done(function (data) {
         cb(data);
@@ -368,13 +255,13 @@ function searchSalesAmount(params, cb) {
         $('#salesAmount .overlay').hide();
     });
 }
-function searchSalesAmountByClient(params, cb) {
+function searchSalesAmountByClient(cb) {
     $('#salesAmount .overlay').show();
     $.getJSON(
         '/dashboard/telemetry/SalesAmountByClient',
         {
-            measureFrom: moment(params.measureFrom).toISOString(),
-            measureThrough: moment(params.measureThrough).toISOString()
+            measureFrom: $('#salesAmount .daterange').data('daterangepicker').startDate.toISOString(),
+            measureThrough: $('#salesAmount .daterange').data('daterangepicker').endDate.toISOString()
         }
     ).done(function (data) {
         cb(data);
@@ -384,13 +271,13 @@ function searchSalesAmountByClient(params, cb) {
         $('#salesAmount .overlay').hide();
     });
 }
-function searchSalesAmountByPaymentMethod(params, cb) {
+function searchSalesAmountByPaymentMethod(cb) {
     $('#salesAmount .overlay').show();
     $.getJSON(
         '/dashboard/telemetry/SalesAmountByPaymentMethod',
         {
-            measureFrom: moment(params.measureFrom).toISOString(),
-            measureThrough: moment(params.measureThrough).toISOString()
+            measureFrom: $('#salesAmount .daterange').data('daterangepicker').startDate.toISOString(),
+            measureThrough: $('#salesAmount .daterange').data('daterangepicker').endDate.toISOString()
         }
     ).done(function (data) {
         cb(data);
@@ -400,13 +287,13 @@ function searchSalesAmountByPaymentMethod(params, cb) {
         $('#salesAmount .overlay').hide();
     });
 }
-function searchSalesAmountBySeller(params, cb) {
+function searchSalesAmountBySeller(cb) {
     $('#salesAmount .overlay').show();
     $.getJSON(
         '/dashboard/telemetry/SalesAmountBySeller',
         {
-            measureFrom: moment(params.measureFrom).toISOString(),
-            measureThrough: moment(params.measureThrough).toISOString()
+            measureFrom: $('#salesAmount .daterange').data('daterangepicker').startDate.toISOString(),
+            measureThrough: $('#salesAmount .daterange').data('daterangepicker').endDate.toISOString()
         }
     ).done(function (data) {
         cb(data);
@@ -416,13 +303,13 @@ function searchSalesAmountBySeller(params, cb) {
         $('#salesAmount .overlay').hide();
     });
 }
-function searchNumOrderItems(params, cb) {
+function searchNumOrderItems(cb) {
     $('#numOrderItems .overlay').show();
     $.getJSON(
         '/dashboard/telemetry/NumOrderItems',
         {
-            measureFrom: moment(params.measureFrom).toISOString(),
-            measureThrough: moment(params.measureThrough).toISOString()
+            measureFrom: $('#numOrderItems .daterange').data('daterangepicker').startDate.toISOString(),
+            measureThrough: $('#numOrderItems .daterange').data('daterangepicker').endDate.toISOString()
         }
     ).done(function (data) {
         cb(data);
@@ -432,13 +319,13 @@ function searchNumOrderItems(params, cb) {
         $('#numOrderItems .overlay').hide();
     });
 }
-function searchNumOrderItemsByClient(params, cb) {
+function searchNumOrderItemsByClient(cb) {
     $('#numOrderItems .overlay').show();
     $.getJSON(
         '/dashboard/telemetry/NumOrderItemsByClient',
         {
-            measureFrom: moment(params.measureFrom).toISOString(),
-            measureThrough: moment(params.measureThrough).toISOString()
+            measureFrom: $('#numOrderItems .daterange').data('daterangepicker').startDate.toISOString(),
+            measureThrough: $('#numOrderItems .daterange').data('daterangepicker').endDate.toISOString()
         }
     ).done(function (data) {
         cb(data);
@@ -448,13 +335,13 @@ function searchNumOrderItemsByClient(params, cb) {
         $('#numOrderItems .overlay').hide();
     });
 }
-function searchNumOrderItemsByPaymentMethod(params, cb) {
+function searchNumOrderItemsByPaymentMethod(cb) {
     $('#numOrderItems .overlay').show();
     $.getJSON(
         '/dashboard/telemetry/NumOrderItemsByPaymentMethod',
         {
-            measureFrom: moment(params.measureFrom).toISOString(),
-            measureThrough: moment(params.measureThrough).toISOString()
+            measureFrom: $('#numOrderItems .daterange').data('daterangepicker').startDate.toISOString(),
+            measureThrough: $('#numOrderItems .daterange').data('daterangepicker').endDate.toISOString()
         }
     ).done(function (data) {
         cb(data);
@@ -464,13 +351,13 @@ function searchNumOrderItemsByPaymentMethod(params, cb) {
         $('#numOrderItems .overlay').hide();
     });
 }
-function searchNumOrderItemsBySeller(params, cb) {
+function searchNumOrderItemsBySeller(cb) {
     $('#numOrderItems .overlay').show();
     $.getJSON(
         '/dashboard/telemetry/NumOrderItemsBySeller',
         {
-            measureFrom: moment(params.measureFrom).toISOString(),
-            measureThrough: moment(params.measureThrough).toISOString()
+            measureFrom: $('#numOrderItems .daterange').data('daterangepicker').startDate.toISOString(),
+            measureThrough: $('#numOrderItems .daterange').data('daterangepicker').endDate.toISOString()
         }
     ).done(function (data) {
         cb(data);
@@ -480,13 +367,13 @@ function searchNumOrderItemsBySeller(params, cb) {
         $('#numOrderItems .overlay').hide();
     });
 }
-function searchNumPlaceOrder(params, cb) {
+function searchNumPlaceOrder(cb) {
     $('#numPlaceOrder .overlay').show();
     $.getJSON(
         '/dashboard/telemetry/NumPlaceOrderByStatus',
         {
-            measureFrom: moment(params.measureFrom).toISOString(),
-            measureThrough: moment(params.measureThrough).toISOString()
+            measureFrom: $('#numPlaceOrder .daterange').data('daterangepicker').startDate.toISOString(),
+            measureThrough: $('#numPlaceOrder .daterange').data('daterangepicker').endDate.toISOString()
         }
     ).done(function (data) {
         cb(data);
