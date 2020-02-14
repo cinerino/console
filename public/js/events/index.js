834bd6ddc9087357d1d18116287bcf34bdae8d98
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-02-14 10:17:38 +0000

    ### Changed - イベント検索を調整

--- public/js/events/index.js
+++ public/js/events/index.js
@@ -1,6 +1,6 @@
 
 $(function () {
-    $("#events-table").DataTable({
+    var table = $("#events-table").DataTable({
         processing: true,
         serverSide: true,
         ajax: {
@@ -48,17 +48,19 @@ $(function () {
                         + '<img src="' + thumbnailImageUrl + '" alt="Product Image" class="img-size-50">'
                         + '</div>'
                         + '<div class="product-info">'
+                        + '<span class="badge badge-secondary ' + data.typeOf + '">' + data.typeOf + '</span>'
+                        + '<br>'
+                        + '<span class="badge badge-secondary ' + data.eventStatus + '">' + data.eventStatus + '</span>'
+                        + '<br>'
                         + '<a target="_blank" href="/projects/' + PROJECT_ID + '/events/' + data.id + '">'
                         + data.name.ja
                         // + '<span class="badge badge-warning float-right">' + data.maximumAttendeeCapacity + ' seats</span>'
                         + '</a>'
                         + '<span class="product-description">' + alternativeHeadline + '</span>'
                         + data.startDate + ' - ' + data.endDate + '<br>'
-                        + '<i class="fa fa-user"></i> ' + data.maximumAttendeeCapacity + ' maximum'
-                        + '<br>'
-                        + '<i class="fa fa-user"></i> ' + data.remainingAttendeeCapacity + ' remaining'
-                        + '<br>'
-                        + '<span class="badge badge-secondary ' + data.eventStatus + '">' + data.eventStatus + '</span>'
+                        // + '<i class="fa fa-user"></i> ' + data.maximumAttendeeCapacity + ' maximum'
+                        // + '<br>'
+                        + '<i class="fa fa-user"></i> ' + data.remainingAttendeeCapacity + '/' + data.maximumAttendeeCapacity + ' remaining'
                         + '<br>'
                         + '</div>'
                         + '</li>'
@@ -68,19 +70,55 @@ $(function () {
             {
                 data: null,
                 render: function (data, type, row) {
-                    return '<ul class="list-unstyled">'
-                        + '<li>' + data.superEvent.location.name.ja + '</li>'
-                        + '<li>' + data.location.name.ja + '</li>'
-                        + '</ul>';
+                    var html = '<ul class="list-unstyled">'
+                        + '<li>' + '<span class="badge badge-secondary ' + data.location.typeOf + '">' + data.location.typeOf + '</span></li>'
+                        + '<li>' + data.location.branchCode + '</li>'
+                        + '<li>' + data.location.name.ja + '</li>';
+
+
+                    html += '<li>';
+                    html += ' <a href="javascript:void(0)" class="btn btn-outline-primary btn-sm showLocation" data-id="' + data.id + '">詳しく</a>';
+                    html += '</li>';
+
+                    html += '</ul>';
+
+                    return html;
                 }
             },
             {
                 data: null,
                 render: function (data, type, row) {
-                    return '<ul class="list-unstyled">'
+                    var html = '<ul class="list-unstyled">'
+                        + '<li>' + '<span class="badge badge-secondary ' + data.superEvent.typeOf + '">' + data.superEvent.typeOf + '</span></li>'
+                        + '<li>' + data.superEvent.id + '</li>'
+                        + '<li>' + data.superEvent.name.ja + '</li>';
+
+                    html += '<li>';
+                    html += ' <a href="javascript:void(0)" class="btn btn-outline-primary btn-sm showSuperEvent" data-id="' + data.id + '">詳しく</a>';
+                    html += '</li>';
+
+                    html += '</ul>';
+
+                    return html;
+                }
+            },
+            {
+                data: null,
+                render: function (data, type, row) {
+                    var html = '<ul class="list-unstyled">'
+                        + '<li>' + '<span class="badge badge-secondary ' + data.workPerformed.typeOf + '">' + data.workPerformed.typeOf + '</span></li>'
+                        + '<li>' + data.workPerformed.id + '</li>'
+                        + '<li>' + data.workPerformed.identifier + '</li>'
                         + '<li>' + data.workPerformed.name + '</li>'
-                        + '<li>' + moment.duration(data.workPerformed.duration).asMinutes() + ' minutes</li>'
-                        + '</ul>';
+                        + '<li>' + moment.duration(data.workPerformed.duration).asMinutes() + ' minutes</li>';
+
+                    html += '<li>';
+                    html += ' <a href="javascript:void(0)" class="btn btn-outline-primary btn-sm showWorkPerformed" data-id="' + data.id + '">詳しく</a>';
+                    html += '</li>';
+
+                    html += '</ul>';
+
+                    return html;
                 }
             },
             {
@@ -90,12 +128,19 @@ $(function () {
                         data.offers = { name: {} };
                     }
 
-                    return '<ul class="list-unstyled">'
+                    var html = '<ul class="list-unstyled">'
                         + '<li><span class="badge badge-secondary">ID</span> ' + String(data.offers.id) + '</li>'
                         + '<li><span class="badge badge-secondary">NAME</span> ' + String(data.offers.name.ja) + '</li>'
                         + '<li><span class="badge badge-secondary">AVBL</span> ' + String(data.offers.availability) + '</li>'
                         + '<li><span class="badge badge-secondary">VALID</span> ' + String(data.offers.validFrom) + ' - ' + String(data.offers.validThrough) + '</li>'
-                        + '</ul>';
+
+                    html += '<li>';
+                    html += ' <a href="javascript:void(0)" class="btn btn-outline-primary btn-sm showOffers" data-id="' + data.id + '">詳しく</a>';
+                    html += '</li>';
+
+                    html += '</ul>';
+
+                    return html;
                 }
             }
         ]
@@ -109,6 +154,41 @@ $(function () {
             format: 'YYYY-MM-DDTHH:mm:ssZ'
         }
     })
+
+    $(document).on('click', '.showLocation', function () {
+        showDetails($(this).data('id'), 'location');
+    });
+
+    $(document).on('click', '.showSuperEvent', function () {
+        showDetails($(this).data('id'), 'superEvent');
+    });
+
+    $(document).on('click', '.showWorkPerformed', function () {
+        showDetails($(this).data('id'), 'workPerformed');
+    });
+
+    $(document).on('click', '.showOffers', function () {
+        showDetails($(this).data('id'), 'offers');
+    });
+
+    function showDetails(id, propertyName) {
+        var events = table
+            .rows()
+            .data()
+            .toArray();
+        var event = events.find(function (e) {
+            return e.id === id
+        })
+
+        var modal = $('#modal-event');
+        var title = 'Event `' + event.id + '` Location';
+        var body = '<textarea rows="25" class="form-control" placeholder="" disabled="">'
+            + JSON.stringify(event[propertyName], null, '\t')
+            + '</textarea>';
+        modal.find('.modal-title').html(title);
+        modal.find('.modal-body').html(body);
+        modal.modal();
+    }
 
     // イベント在庫仕入れ
     $('button.importScreeningEvents').click(function () {
@@ -128,7 +208,21 @@ $(function () {
                 data: $('form').serialize()
             }).done(function (tasks) {
                 console.log(tasks);
-                alert('インポートを開始しました');
+
+                if (Array.isArray(tasks)) {
+                    var modal = $('#modal-sm');
+                    var title = 'インポートを開始しました(' + tasks.length + 'つのタスクが作成されました' + ')';
+                    var body = tasks.map(function (task) {
+                        var href = '/projects/' + PROJECT_ID + '/tasks/' + task.id + '?name=' + task.name;
+                        return task.id + ' <a target="_blank" href="' + href + '">タスクを確認</a>';
+                    }).join('<br>');
+                    ;
+                    modal.find('.modal-title').html(title);
+                    modal.find('.modal-body').html(body);
+                    modal.modal();
+                }
+
+                // alert('インポートを開始しました');
             }).fail(function (xhr) {
                 var res = $.parseJSON(xhr.responseText);
                 alert(res.error.message);
