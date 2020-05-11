5611007c56e5f488e4b067e0824124b52fe39f3a
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-05-11 23:56:10 +0000

    ### Changed - UI調整

--- dst/factory/timeline.js
+++ dst/factory/timeline.js
@@ -167,78 +167,92 @@ function createFromAction(params) {
             actionName = a.typeOf;
     }
     let object = { name: 'Unknown' };
-    if (a.object !== undefined && a.object !== null) {
-        let url;
-        if (typeof a.object.typeOf === 'string' && typeof a.object.id === 'string') {
-            url = `/projects/${params.project.id}/resources/${a.object.typeOf}/${a.object.id}`;
-        }
-        if (Array.isArray(a.object)) {
-            switch (a.object[0].typeOf) {
-                case 'PaymentMethod':
-                    object = { name: a.object[0].paymentMethod.name };
-                    break;
-                case cinerinoapi.factory.actionType.PayAction:
-                    object = { name: a.object[0].object.paymentMethod.typeOf };
-                    break;
-                default:
-                    object = a.object[0].typeOf;
+    try {
+        if (a.object !== undefined && a.object !== null) {
+            let url;
+            if (typeof a.object.typeOf === 'string' && typeof a.object.id === 'string') {
+                url = `/projects/${params.project.id}/resources/${a.object.typeOf}/${a.object.id}`;
             }
-        }
-        else {
-            switch (a.object.typeOf) {
-                case cinerinoapi.factory.action.authorize.offer.seatReservation.ObjectType.SeatReservation:
-                    object = { name: '座席予約' };
-                    break;
-                case cinerinoapi.factory.paymentMethodType.CreditCard:
-                    object = { name: 'クレジットカード決済' };
-                    break;
-                case cinerinoapi.factory.paymentMethodType.Account:
-                    object = { name: '口座決済' };
-                    break;
-                case cinerinoapi.factory.action.authorize.award.point.ObjectType.PointAward:
-                    object = { name: 'ポイントインセンティブ' };
-                    break;
-                case 'Order':
-                    url = `/projects/${params.project.id}/orders/${a.object.orderNumber}`;
-                    object = { name: '注文' };
-                    break;
-                case 'OwnershipInfo':
-                    object = { name: '所有権' };
-                    break;
-                case cinerinoapi.factory.action.transfer.give.pointAward.ObjectType.PointAward:
-                    object = { name: 'ポイント' };
-                    break;
-                case cinerinoapi.factory.actionType.SendAction:
-                    if (a.object.typeOf === 'Order') {
-                        object = { name: '配送' };
-                    }
-                    else if (a.object.typeOf === cinerinoapi.factory.creativeWorkType.EmailMessage) {
-                        object = { name: '送信' };
-                    }
-                    else {
-                        object = { name: '送信' };
-                    }
-                    break;
-                case cinerinoapi.factory.creativeWorkType.EmailMessage:
-                    object = { name: 'Eメール' };
-                    break;
-                case 'PaymentMethod':
-                    object = { name: a.object.object[0].paymentMethod.name };
-                    break;
-                case cinerinoapi.factory.actionType.PayAction:
-                    object = { name: a.object.object[0].paymentMethod.typeOf };
-                    break;
-                case cinerinoapi.factory.chevre.transactionType.Reserve:
-                    object = { name: '予約取引' };
-                    break;
-                case cinerinoapi.factory.programMembership.ProgramMembershipType.ProgramMembership:
-                    object = { name: '会員プログラム' };
-                    break;
-                default:
-                    object = { name: a.object.typeOf };
+            if (Array.isArray(a.object)) {
+                switch (a.object[0].typeOf) {
+                    case 'PaymentMethod':
+                        object = { name: a.object[0].paymentMethod.name };
+                        break;
+                    case cinerinoapi.factory.actionType.PayAction:
+                        object = { name: a.object[0].object.paymentMethod.typeOf };
+                        break;
+                    default:
+                        object = a.object[0].typeOf;
+                }
             }
+            else {
+                switch (a.object.typeOf) {
+                    case cinerinoapi.factory.chevre.offerType.Offer:
+                        object = { name: 'オファー' };
+                        break;
+                    case cinerinoapi.factory.action.authorize.offer.seatReservation.ObjectType.SeatReservation:
+                        object = { name: '座席予約' };
+                        break;
+                    case cinerinoapi.factory.paymentMethodType.CreditCard:
+                        object = { name: 'クレジットカード決済' };
+                        break;
+                    case cinerinoapi.factory.paymentMethodType.Account:
+                        object = { name: '口座決済' };
+                        break;
+                    case 'PaymentCard':
+                        object = { name: '決済カード' };
+                        break;
+                    case cinerinoapi.factory.action.authorize.award.point.ObjectType.PointAward:
+                        object = { name: 'ポイントインセンティブ' };
+                        break;
+                    case 'Order':
+                        url = `/projects/${params.project.id}/orders/${a.object.orderNumber}`;
+                        object = { name: '注文' };
+                        break;
+                    case 'OwnershipInfo':
+                        object = { name: '所有権' };
+                        break;
+                    case cinerinoapi.factory.action.transfer.give.pointAward.ObjectType.PointAward:
+                        object = { name: 'ポイント' };
+                        break;
+                    case cinerinoapi.factory.actionType.SendAction:
+                        if (a.object.typeOf === 'Order') {
+                            object = { name: '配送' };
+                        }
+                        else if (a.object.typeOf === cinerinoapi.factory.creativeWorkType.EmailMessage) {
+                            object = { name: '送信' };
+                        }
+                        else {
+                            object = { name: '送信' };
+                        }
+                        break;
+                    case cinerinoapi.factory.creativeWorkType.EmailMessage:
+                        object = { name: 'Eメール' };
+                        break;
+                    case 'PaymentMethod':
+                        object = { name: a.object.object[0].paymentMethod.name };
+                        break;
+                    case cinerinoapi.factory.actionType.PayAction:
+                        object = { name: a.object.object[0].paymentMethod.typeOf };
+                        break;
+                    case cinerinoapi.factory.chevre.transactionType.Reserve:
+                        object = { name: '予約取引' };
+                        break;
+                    case cinerinoapi.factory.chevre.transactionType.MoneyTransfer:
+                        object = { name: '通貨転送取引' };
+                        break;
+                    case cinerinoapi.factory.programMembership.ProgramMembershipType.ProgramMembership:
+                        object = { name: '会員プログラム' };
+                        break;
+                    default:
+                        object = { name: a.object.typeOf };
+                }
+            }
+            object.url = url;
         }
-        object.url = url;
+    }
+    catch (error) {
+        // no op
     }
     if (a.typeOf === cinerinoapi.factory.actionType.MoneyTransfer) {
         const amount = a.amount;
